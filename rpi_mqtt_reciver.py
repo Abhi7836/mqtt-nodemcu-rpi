07d2c2e146b0a38f4b2ebb1e0855b1ba400db4c1
S Abhishekk lal Naikk <39412350+Abhi7836@users.noreply.github.com>
2019-10-10 13:37:11 +0530

    Update rpi_mqtt_reciver.py

--- rpi_mqtt_reciver.py
+++ rpi_mqtt_reciver.py
@@ -1,34 +1,54 @@
 import csv
+from time import sleep, strftime, time
 import paho.mqtt.client as mqtt
-mqtt_username = "username"
-mqtt_password = "password"
-mqtt_broker_ip = "192.168.43.20"
+mqtt_username = "nodemcu"
+mqtt_password = "12345"
+mqtt_broker_ip = "192.168.43.162"
 client = mqtt.Client()
 client.username_pw_set(mqtt_username, mqtt_password)
 val=[]
+with open('MPU_val.csv', 'a') as writeFile:
+        writeFile.write("date"+","+"time"+","+"AX"+","+"AY"+","+"AZ"+","+"GX"+","+"GY"+","+"GZ"+","+"MX"+","+"MY"+","+"MZ"+","+"temp"+"\n")
+########################################################################
 def on_connect(client, userdata, flags, rc):
     print("Connected!", str(rc))
-    client.subscribe("esp8266/roll")
-    client.subscribe("esp8266/pitch")
-    client.subscribe("esp8266/yaw")
+    client.subscribe("esp8266/ax")
+    client.subscribe("esp8266/ay")
+    client.subscribe("esp8266/az")
+    client.subscribe("esp8266/gx")
+    client.subscribe("esp8266/gy")
+    client.subscribe("esp8266/gz")
+    client.subscribe("esp8266/mx")
+    client.subscribe("esp8266/my")
+    client.subscribe("esp8266/mz")
+    client.subscribe("esp8266/t")
+#######################################################################
 def on_message(client, userdata, msg):
-    #print("Topic: ", msg.topic + "\nMessage: " + str(msg.payload))
-    if msg.topic == "esp8266/roll":
-        #print("roll   " + msg.payload)
-        val.append(str(msg.payload))
-
-    if msg.topic == "esp8266/pitch":
-        #print("pitch  "+msg.payload)
-        val.append(str(msg.payload))
-    if msg.topic == "esp8266/yaw":
-        #print("yaw  "+msg.payload)
-        val.append(str(msg.payload))
+    if msg.topic == "esp8266/ax":
+        val.append(float(msg.payload))
+    if msg.topic == "esp8266/ay":
+        val.append(float(msg.payload))
+    if msg.topic == "esp8266/az":
+        val.append(float(msg.payload))
+    if msg.topic == "esp8266/gx":
+        val.append(float(msg.payload))
+    if msg.topic == "esp8266/gy":
+        val.append(float(msg.payload))
+    if msg.topic == "esp8266/gz":
+        val.append(float(msg.payload))
+    if msg.topic == "esp8266/mx":
+        val.append(float(msg.payload))
+    if msg.topic == "esp8266/my":
+        val.append(float(msg.payload))
+    if msg.topic == "esp8266/mz":
+        val.append(float(msg.payload))
+    if msg.topic == "esp8266/t":
+        val.append(float(msg.payload))
         print(val)
-        with open('people.csv', 'a') as writeFile:
-            writer = csv.writer(writeFile)
-            writer.writerows(val)
-        writeFile.close()
+        with open('MPU_val.csv 'a') as writeFile:
+            writeFile.write(strftime("%Y-%m-%d , %H:%M:%S")+" ,"+str(val[0])+", "+str(val[1])+" ,"+str(val[2])+" ,"+str(val[3])+", "+str(val[4])+" ,"+str(val[5])+" ,"+str(val[6])+", "+str(val[7])+" ,"+str(val[8])+" ,"+str(val[9])+"\n")
         val.clear()
+ ##############################################################################       
 client.on_connect = on_connect
 client.on_message = on_message
 
